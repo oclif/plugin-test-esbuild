18b908292e98e221fbec030a3c5b5937491c498b
Mike Donnalley <mdonnalley@salesforce.com>
2024-02-15 14:15:56 -0700

    feat: add hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Hello from './commands/hello'
 import HelloWorld from './commands/hello/world'
+export {default as INIT_HOOK} from './hooks/init/init'
 
 export const COMMANDS = {
   hello: Hello,
+  'hello:alias': HelloWorld,
   'hello:world': HelloWorld,
 }
