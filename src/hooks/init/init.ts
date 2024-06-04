1d50fe041da3ec6d1d470af429f58c6a7968b7df
Mike Donnalley <mdonnalley@salesforce.com>
2024-06-04 10:46:41 -0600

    feat: core v4

--- src/hooks/init/init.ts
+++ src/hooks/init/init.ts
@@ -1,7 +1,7 @@
 import {Hook, ux} from '@oclif/core'
 
 const hook: Hook<'init'> = async function () {
-  ux.log('Greetings! from plugin-test-esbuild init hook')
+  ux.stdout('Greetings! from plugin-test-esbuild init hook')
 }
 
 export default hook
